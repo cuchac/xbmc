22df6e8507b4cc0ce1ebc4a7c58f22ca8a004294
Garrett Brown <themagnificentmrb@gmail.com>
2018-07-14 16:52:11 -0700

    Environment WIP

--- xbmc/cores/RetroPlayer/RetroPlayer.cpp
+++ xbmc/cores/RetroPlayer/RetroPlayer.cpp
@@ -15,6 +15,7 @@
 #include "addons/AddonManager.h"
 #include "cores/DataCacheCore.h"
 #include "cores/IPlayerCallback.h"
+#include "cores/RetroPlayer/environment/Environment.h"
 #include "cores/RetroPlayer/guibridge/GUIGameRenderManager.h"
 #include "cores/RetroPlayer/guiplayback/GUIPlaybackControl.h"
 #include "cores/RetroPlayer/playback/IPlayback.h"
